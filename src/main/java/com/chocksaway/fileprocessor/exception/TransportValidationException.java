f314e426e112cfc95ef49865f11c9a581b1106a8
milesd <milesd@chocksaway.com>
2024-03-02 23:17:55 +0000

    Initial IP address validation code.

--- src/main/java/com/chocksaway/fileprocessor/exception/TransportValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.chocksaway.fileprocessor.exception;
-
-public class TransportValidationException extends Exception {
-    public TransportValidationException(String message) {
-        super(message);
-    }
-}
